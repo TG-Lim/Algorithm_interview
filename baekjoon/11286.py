bc380685662b130bbe647200286a1efcf7e4442c
TaekyuIm <kyu5787@naver.com>
2024-04-18 00:15:16 +0900

    Silver1 자료구조

---
+++ baekjoon/11286.py
@@ -0,0 +1,18 @@
+# Silver 1
+import heapq
+import sys
+N = int(input())
+heap = []
+result = []
+for _ in range(N):
+    n = int(sys.stdin.readline().strip())
+    if n == 0:
+        if not heap: # 리스트가 비어 있는 경우
+            result.append("0\n")
+        else:
+            small = heapq.heappop(heap)
+            result.append(f"{small[1]}\n") # 최소값 출력
+    else:
+        heapq.heappush(heap, (abs(n),n))
+
+sys.stdout.write(''.join(result))
