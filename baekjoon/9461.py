4d1a590f5ce29b61e80e18dbf124c45033175b08
임태규 <kyu5787@naver.com>
2024-05-03 08:54:38 +0900

    Silver3 DP

--- baekjoon/9461.py
+++ baekjoon/9461.py
@@ -1,5 +1,18 @@
 # Silver 3
-
+# DP
 array = [0 for _ in range(101)]
 array[1] = 1
 array[2] = 1
+array[3] = 1
+array[4] = 2
+array[5] = 2
+for i in range(6, 101):
+    array[i] = array[i-1] + array[i-5]
+
+N = int(input())
+test_case = []
+for _ in range(N):
+    test = int(input())
+    test_case.append(test)
+for test in test_case:
+    print(array[test])
