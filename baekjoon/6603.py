96074132f37b913c1ad2408eb77e1169af0390cf
TaeKyuIm <kyu5787@naver.com>
2024-05-27 13:45:22 +0900

    Silver2 구현

---
+++ baekjoon/6603.py
@@ -0,0 +1,18 @@
+from itertools import combinations
+import sys
+input = sys.stdin.readline
+arraies = []
+while True:
+    array = list(map(int, input().strip().split()))
+    if array[0] == 0:
+        break
+    arraies.append(array)
+result = []
+for array in arraies:
+    lotto = list(combinations(array[1:], 6))
+    for l in lotto:
+        list_l = list(l)
+        list_l.append('\n')
+        sys.stdout.write(' '.join(str(l2) for l2 in list_l))
+    print('')
+    
