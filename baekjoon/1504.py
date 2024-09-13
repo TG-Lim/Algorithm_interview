87eafdc079317ace5ed8d2641fb01862da7ab7ce
TaekyuIm <kyu5787@naver.com>
2024-09-13 23:49:04 +0900

    Gold4 다익스트라

--- baekjoon/1504.py
+++ baekjoon/1504.py
@@ -1,19 +1,49 @@
 # Gold 4
 import sys
+import heapq
 
 input = sys.stdin.readline
-inf = int(1e6)
-N, E = map(int, input().strip().split())
-graph = [[inf]*(N+1) for _ in range(N+1)]
+inf = float('inf')
 
-for i in range(1, N+1):
-    graph[i][i] = 0
+def dijkstra(departure, arrival):
+    if departure == arrival:
+        return 0
+    distance = [inf]*(N+1)
+    q = []
+    heapq.heappush(q, (0, departure)) # 거리, 지점
+    distance[departure] = 0 # 시작점 거리를 0
+    while q:
+        dist, now = heapq.heappop(q)
 
-for _ in range(E):
-    a, b, c = map(int, input().strip().split())
-    graph[a][b] = c
-    graph[b][a] = c
+        if distance[now] < dist: # 이미 최단거리
+            continue
 
-v1, v2 = map(int, input().strip().split())
+        for i in range(1, N+1):
+            cost = dist + graph[now][i]
+            if cost < distance[i]:
+                distance[i] = cost
+                heapq.heappush(q, (cost, i))
 
-print(graph)
+    return distance[arrival]
+
+if __name__ == '__main__':
+    N, E = map(int, input().strip().split())
+    graph = [[inf]*(N+1) for _ in range(N+1)]
+
+    for i in range(1, N+1):
+        graph[i][i] = 0
+
+    for _ in range(E):
+        a, b, c = map(int, input().strip().split())
+        graph[a][b] = c
+        graph[b][a] = c
+
+    v1, v2 = map(int, input().strip().split())
+
+    case1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, N)
+    case2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, N)
+
+    if case1 != inf or case2 != inf:
+        print(min(case1, case2))
+    else:
+        print(-1)
